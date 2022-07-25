b465a102f972e3e555e60972da4bcee7e7445391
Col0ring <1561999073@qq.com>
2022-07-25 19:38:39 +0800

    fix: test env

--- libs/test.js
+++ libs/test.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['./basic'],
   overrides: [
     {
       plugins: ['jest'],
       files: ['*.{test,spec}.{js,jsx,ts,tsx}'],
       extends: [
+        './basic',
         'plugin:jest/recommended',
         'plugin:jest/style',
         'plugin:jest-dom/recommended',
